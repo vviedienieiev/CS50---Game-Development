271d2a37b5646560b9128777bd11b444165fe7ab
VolodymyrViedienieiev <volodymyr.viedienieiev@railsware.com>
2023-09-17 13:22:58 +0300

    add lessons 7-9

---
+++ CS50 - Game Dev/Lesson 9 - 3D Helicopter/Assets/Resources/Scripts/RewardSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewardSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	public int DiamondChance = 5;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnRewards());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnRewards() {
+		while (true) {
+
+			// number of coins we could spawn vertically
+			int coinsThisRow = Random.Range(1, 4);
+
+			// instantiate all coins in this row separated by some random amount of space
+			for (int i = 0; i < coinsThisRow; i++) {
+				if (Random.Range(0, 100) <= DiamondChance){
+					Instantiate(prefabs[1], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+				}
+				else
+				{
+					Instantiate(prefabs[0], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+				}
+			}
+
+			// pause 1-5 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(1, 5));
+		}
+	}
+}
---
+++ CS50 - Game Dev/Lesson 9 - 3D Helicopter/Assets/Resources/Scripts/SkySpeed.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class SkyText : MonoBehaviour {
+
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = "Speed: " + SkyscraperSpawner.speed;
+	}
+}
